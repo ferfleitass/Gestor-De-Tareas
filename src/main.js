681e1e295a0031c4d4b826fa2f71f4f7654fd62b
fernando <fleitasbaez55@gmail.com>
2024-11-12 10:37:16 -0300

    Agregar Bootstrap al proyecto para mejorar el diseño responsivo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//Importamos el archivo CSS de bootstrap
+import 'bootstrap/dist/css/bootstrap.min.cs'
+
 createApp(App).mount('#app')
